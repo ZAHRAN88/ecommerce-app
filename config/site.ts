0011893ab420475e9f14fba4c8915702309bbe30
ZAHRAN88 <124225355+ZAHRAN88@users.noreply.github.com>
2024-02-23 22:58:38 +0200

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "E-commerce App",
+  name: "E-commerce",
   description: "Discover a vast selection of products ranging from electronics and gadgets to fashion, home decor, beauty essentials, and beyond. Whether you're searching for the latest tech innovations, trendy fashion pieces, or unique gifts, we have something for everyone.",
   footer: [
     { name: "Home", href: "/" },
