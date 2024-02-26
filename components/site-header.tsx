663db425cf0f0ee2032cdd5a18c02c8faf909afa
ZAHRAN88 <124225355+ZAHRAN88@users.noreply.github.com>
2024-02-27 01:48:05 +0200

    nav scroll handle

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+import React, { useState, useEffect } from 'react';
 import Link from "next/link"
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { Edit, Home, ShoppingBag, ShoppingCart } from "lucide-react"
@@ -15,51 +15,63 @@ export function SiteHeader() {
   const searchParams = useSearchParams()
   const defaultSearchQuery = searchParams.get("search") ?? ""
   const { cartCount } = useShoppingCart()
- 
 
-  const  pathname  = usePathname()
-  if (pathname.startsWith ("/studio")) return null
+  const [isScrolled, setIsScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const isCurrentlyScrolled = scrollTop > 0;
+      setIsScrolled(isCurrentlyScrolled);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    
+    
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const pathname = usePathname();
+  if (pathname.startsWith("/studio")) return null;
 
   function onSubmit(event: React.SyntheticEvent<HTMLFormElement>) {
     event.preventDefault()
-   const formData  =new FormData(event.currentTarget)
-    const searchQuery  =formData.get("search") 
+    const formData = new FormData(event.currentTarget)
+    const searchQuery = formData.get("search")
     router.replace(`/shop?search=${searchQuery}`)
   }
 
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background ">
-      <div className="mx-auto flex h-16 max-w-6xl items-center justify-between space-x-4 px-6 sm:space-x-0">
-        <MainNav />
+    <header className={`sticky top-0 z-40 w-full border-b bg-background ${isScrolled ? 'scrolled' : ''}`}>
+      <div className="mx-auto flex h-16 max-w-6xl items-center justify-between gap-x-4 space-x-4 px-6 ">
+        <div className="sm:mr-3 pr-6">
+          <MainNav />
+        </div>
 
-
- {/* if the href contains ==="/shop" */}
-
-        { pathname === "/shop" && (
-        <form className="hidden items-center lg:inline-flex">
-          <Input
-            id="search"
-            name="search"
-            type="search"
-            autoComplete="off"
-            placeholder="Search products..."
-            className="h-9 lg:w-[300px]"
-            defaultValue={defaultSearchQuery}
-          />
-        </form>)
-}
-        <div className="flex items-center space-x-1">
-        <Link href="/">
+        {pathname === "/shop" && (
+          <form className="hidden items-center lg:inline-flex" onSubmit={onSubmit}>
+            <Input
+              id="search"
+              name="search"
+              type="search"
+              autoComplete="off"
+              placeholder="Search products..."
+              className="h-9 lg:w-[300px]"
+              defaultValue={defaultSearchQuery}
+            />
+          </form>
+        )}
+        <div className="flex items-center space-x-1 sm:ml-8">
+          <Link href="/">
             <Button size="sm" variant="ghost" title="Home">
               <Home className="h-5 w-5" />
-              
               <span className="sr-only">Home</span>
             </Button>
           </Link>
-        <Link href="/shop">
+          <Link href="/shop">
             <Button size="sm" variant="ghost" title="Shop">
               <ShoppingCart className="h-5 w-5" />
-              
               <span className="sr-only">Shop</span>
             </Button>
           </Link>
@@ -70,18 +82,16 @@ export function SiteHeader() {
               <span className="sr-only">Cart</span>
             </Button>
           </Link>
-          
+
           <ThemeToggle />
           {/* if we are in dev mode */}
-          
-          {process.env.NODE_ENV === "development" && (
 
+          {process.env.NODE_ENV === "development" && (
             <Link href="/studio">
               <Button size="sm" variant="ghost">
                 <Edit className="h-5 w-5" />
               </Button>
             </Link>
-
           )}
         </div>
       </div>
