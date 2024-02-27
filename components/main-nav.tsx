1b62e23e96e8d740099ad5b987fb0b2ec52e01f4
ZAHRAN88 <124225355+ZAHRAN88@users.noreply.github.com>
2024-02-27 02:02:27 +0200

    Update main-nav.tsx

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -8,6 +8,10 @@ export function MainNav() {
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="flex  items-center space-x-2">
         <img src="https://i.postimg.cc/t4Rz6jPj/logo-png.png" className="  h-14 w-14" />
+        {/* in large screens display span with the name  */}
+        <span className="hidden md:block text-2xl font-semibold text-gray-800 dark:text-gray-100">
+          {siteConfig.name}
+        </span>
         
       </Link>
     </div>
